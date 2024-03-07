a8da931372cbb606ee33b4cdac33fc429f80906b
luissoy <luisserranomarin@gmail.com>
2024-03-07 18:48:59 +0100

    Initial commit

---
+++ DatabaseLayer/src/test/java/com/dynamiceventmanagement/databaselayer/DatabaseLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamiceventmanagement.databaselayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
