98fc7fd9b570495f05ee180373e87e6dd8166216
luissoy <luisserranomarin@gmail.com>
2024-04-07 14:21:18 +0200

    feat: Custom Exception for Data Not Found

---
+++ DatabaseLayer/src/main/java/com/dynamiceventmanagement/databaselayer/exception/DataNotFoundException.java
@@ -0,0 +1,11 @@
+package com.dynamiceventmanagement.databaselayer.exception;
+
+public class DataNotFoundException extends Exception {
+    public DataNotFoundException() {
+    }
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+
+}
