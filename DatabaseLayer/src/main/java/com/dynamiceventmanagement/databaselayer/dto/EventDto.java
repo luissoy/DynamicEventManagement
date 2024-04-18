fdbfa79c89fc6a988be1981c26756571f18ff10a
luissoy <luisserranomarin@gmail.com>
2024-04-18 19:55:49 +0200

    feat: Event dto new field and updated

--- DatabaseLayer/src/main/java/com/dynamiceventmanagement/databaselayer/dto/EventDto.java
+++ DatabaseLayer/src/main/java/com/dynamiceventmanagement/databaselayer/dto/EventDto.java
@@ -6,12 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class EventDto {
     private String groupId;
-    private Group groupInfo;
+
     private String userId;
+
+    private LocalDateTime dateTime;
 }
