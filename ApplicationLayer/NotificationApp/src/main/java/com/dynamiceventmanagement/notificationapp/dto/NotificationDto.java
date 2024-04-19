4301dd4b35463a36370b2ae21aef8e2bd69b5fa7
luissoy <luisserranomarin@gmail.com>
2024-04-19 18:08:17 +0200

    feat: All the dtos for the database

---
+++ ApplicationLayer/NotificationApp/src/main/java/com/dynamiceventmanagement/notificationapp/dto/NotificationDto.java
@@ -0,0 +1,24 @@
+package com.dynamiceventmanagement.notificationapp.dto;
+
+import com.dynamiceventmanagement.notificationapp.model.Group;
+import com.dynamiceventmanagement.notificationapp.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationDto {
+    private String eventId;
+
+    private User user;
+
+    private User userToNotify;
+
+    private Group group;
+
+    private Object message;
+}
