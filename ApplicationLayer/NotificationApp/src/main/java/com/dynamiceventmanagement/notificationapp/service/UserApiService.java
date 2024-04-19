779353eda0a544dd5aa0f0fa7df474b367fa6015
luissoy <luisserranomarin@gmail.com>
2024-04-19 18:17:39 +0200

    feat: Service for User of the Database Layer App

---
+++ ApplicationLayer/NotificationApp/src/main/java/com/dynamiceventmanagement/notificationapp/service/UserApiService.java
@@ -0,0 +1,39 @@
+package com.dynamiceventmanagement.notificationapp.service;
+
+import com.dynamiceventmanagement.notificationapp.exception.DatabaseApiException;
+import com.dynamiceventmanagement.notificationapp.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class UserApiService {
+    private final RestTemplate restTemplate;
+
+    @Value("${api.database.url}")
+    private String apiUrl;
+
+    @Value("${api.database.endpoints.users}")
+    private String userEndpoint;
+
+    public UserApiService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public User getOne(String userId) throws DatabaseApiException {
+        String url = apiUrl + userEndpoint + "/" + userId;
+        ResponseEntity<?> responseEntity = restTemplate.getForEntity(
+                url,
+                User.class);
+
+        Object responseBody = responseEntity.getBody();
+        if (responseEntity.getStatusCode() != HttpStatus.OK || !(responseBody instanceof User)) {
+            String responseBodyString = (responseBody != null) ? responseBody.toString() : "";
+            throw new DatabaseApiException(responseBodyString);
+        }
+
+        return (User) responseEntity.getBody();
+    }
+}
