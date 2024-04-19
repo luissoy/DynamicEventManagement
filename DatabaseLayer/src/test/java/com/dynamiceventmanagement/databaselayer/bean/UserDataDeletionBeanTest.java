b0ad4a71c71962e14559d4006343f4a49a5dae8d
luissoy <luisserranomarin@gmail.com>
2024-04-19 19:01:24 +0200

    feat: Test for UserDataDeletionBean

---
+++ DatabaseLayer/src/test/java/com/dynamiceventmanagement/databaselayer/bean/UserDataDeletionBeanTest.java
@@ -0,0 +1,59 @@
+package com.dynamiceventmanagement.databaselayer.bean;
+
+import com.dynamiceventmanagement.databaselayer.dto.UserDto;
+import com.dynamiceventmanagement.databaselayer.exception.DataIntegrityException;
+import com.dynamiceventmanagement.databaselayer.exception.DataNotFoundException;
+import com.dynamiceventmanagement.databaselayer.model.User;
+import com.dynamiceventmanagement.databaselayer.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserDataDeletionBeanTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserDataDeletionBean userDataDeletionBean;
+
+    @Test
+    void testDeleteAppParametersFromUsersByAppId() throws DataNotFoundException, DataIntegrityException {
+        // Given
+        String appId = "AppId";
+        String userId1 = "userId1";
+        String userId2 = "userId2";
+
+        List<User> users = new ArrayList<>();
+        Map<String, Map<String, String>> appParameters = new HashMap<>();
+        appParameters.put(appId, new HashMap<>(Map.of("appId", "value")));
+
+        User user1 = new User(userId1, "Username1", appParameters);
+        users.add(user1);
+
+        User user2 = new User(userId2, "Username2", appParameters);
+        users.add(user2);
+
+        // When
+        when(userService.getByAppId(appId)).thenReturn(users);
+        when(userService.update(any(), any())).thenReturn(null);
+
+        // Then
+        userDataDeletionBean.deleteAppParametersFromUsersByAppId(appId);
+
+        verify(userService, times(1)).getByAppId(appId);
+        verify(userService, times(1)).update(eq(userId1), any(UserDto.class));
+        verify(userService, times(1)).update(eq(userId2), any(UserDto.class));
+    }
+}
