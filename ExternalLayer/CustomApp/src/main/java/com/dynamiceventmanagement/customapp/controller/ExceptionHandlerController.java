d2d690cd8d72adc825d6271a31d751979f71fbfb
luissoy <luisserranomarin@gmail.com>
2024-04-20 18:02:01 +0200

    fix: Asyn controller for Custom App

--- ExternalLayer/CustomApp/src/main/java/com/dynamiceventmanagement/customapp/controller/ExceptionHandlerController.java
+++ ExternalLayer/CustomApp/src/main/java/com/dynamiceventmanagement/customapp/controller/ExceptionHandlerController.java
@@ -3,16 +3,18 @@ package com.dynamiceventmanagement.customapp.controller;
 import com.dynamiceventmanagement.customapp.response.StandardErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
 
+    @Async
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleException(Exception e) {
+    public void handleException(Exception e) {
         String message = e.getMessage();
-        return ResponseEntity.
+        ResponseEntity<StandardErrorResponse> responseEntity = ResponseEntity.
                 status(HttpStatus.INTERNAL_SERVER_ERROR).
                 body(new StandardErrorResponse(message, e.getClass().getName()));
     }
