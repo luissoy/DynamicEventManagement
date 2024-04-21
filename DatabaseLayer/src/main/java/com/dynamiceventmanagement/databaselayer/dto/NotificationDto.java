df4bac89584669cdc37130fe4d820c01bfb76a1c
luissoy <luisserranomarin@gmail.com>
2024-04-21 14:49:55 +0200

    feat: Notification dtos provide a list of users to notify

--- DatabaseLayer/src/main/java/com/dynamiceventmanagement/databaselayer/dto/NotificationDto.java
+++ DatabaseLayer/src/main/java/com/dynamiceventmanagement/databaselayer/dto/NotificationDto.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -16,7 +18,7 @@ public class NotificationDto {
 
     private User user;
 
-    private User userToNotify;
+    private List<User> usersToNotify;
 
     private Group group;
 
