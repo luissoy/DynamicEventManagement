17debc2e8849087d78d535e16709b028014ee3e5
luissoy <luisserranomarin@gmail.com>
2024-04-21 14:50:24 +0200

    feat: Exception for Custom App

---
+++ ExternalLayer/CustomApp/src/main/java/com/dynamiceventmanagement/customapp/exception/ExternalDataIntegrityException.java
@@ -0,0 +1,10 @@
+package com.dynamiceventmanagement.customapp.exception;
+
+public class ExternalDataIntegrityException extends Exception {
+    public ExternalDataIntegrityException() {
+    }
+
+    public ExternalDataIntegrityException(String message) {
+        super(message);
+    }
+}
